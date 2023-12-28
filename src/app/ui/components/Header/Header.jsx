6d7901de8d1bfaad695ede805b5f6915ab061a6d
Gaston82 <cabezaartaza@hotmail.com>
2023-12-28 21:56:51 +0100

    Implement SocialNavbar component

--- src/app/ui/components/Header/Header.jsx
+++ src/app/ui/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import facebook from "../../../../../public/assets/img/icons/facebook.svg";
 import twitter from "../../../../../public/assets/img/icons/simple-icons_x.svg";
 import instagram from "../../../../../public/assets/img/icons/mdi_instagram.svg";
 import tiktok from "../../../../../public/assets/img/icons/ic_baseline-tiktok.svg";
+import SocialNavbar from "../SocialNavbar/SocialNavbar";
 
 const Header = () => {
   return (
@@ -32,40 +33,7 @@ const Header = () => {
         priority={true}
         style={{ paddingTop: "40px" }}
       />
-      <nav className="w-36">
-        <ul className="flex justify-between">
-          <li>
-            <Link href={"https://www.facebook.com/DoctorWho/"} target="_blank">
-              <Image
-                src={facebook}
-                alt="facebook logo"
-                height={20}
-                width={20}
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://www.facebook.com/DoctorWho/"} target="_blank">
-              <Image src={twitter} alt="facebook logo" height={20} width={20} />
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://www.facebook.com/DoctorWho/"} target="_blank">
-              <Image
-                src={instagram}
-                alt="facebook logo"
-                height={20}
-                width={20}
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://www.facebook.com/DoctorWho/"} target="_blank">
-              <Image src={tiktok} alt="facebook logo" height={20} width={20} />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <SocialNavbar />
     </header>
   );
 };
