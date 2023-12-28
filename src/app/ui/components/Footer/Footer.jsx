51b24abe8e42ecbc34d018c116af0a6604fddf6a
Gaston82 <cabezaartaza@hotmail.com>
2023-12-28 22:41:56 +0100

    Add white line and icons container

--- src/app/ui/components/Footer/Footer.jsx
+++ src/app/ui/components/Footer/Footer.jsx
@@ -5,9 +5,12 @@ import logoCadena from "../../../../../public/assets/img/logos/logo-cadena.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#040521] text-white h-24 flex justify-between items-center px-16 ">
-      <Image src={logoCadena} alt="Logo cadena " width={30} height={30} />
-      <SocialNavbar />
+    <footer className="bg-[#040521] text-white   flex flex-col ">
+      <div className="border-solid border-2 border-white w-full p-1"></div>
+      <div className="h-16 flex justify-between  px-16 items-center ">
+        <Image src={logoCadena} alt="Logo cadena " width={30} height={30} />
+        <SocialNavbar />
+      </div>
     </footer>
   );
 };
